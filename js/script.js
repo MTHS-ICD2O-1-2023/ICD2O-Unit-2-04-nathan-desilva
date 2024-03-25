bec8748bf30d7ead008e397e43a979c73385955a
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-25 18:33:33 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle () {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const calculateAreaOfTriangle = baseOfTriangle * heightOfTriangle * 0.5
+
+  // output
+  document.getElementById('area-of-triangle').innerHTML = 'Area is: ' + baseOfTriangle * heightOfTriangle * 0.5 +' cm²'
+}
